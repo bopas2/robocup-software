01320b81b131cdd643d8553f5ea3290d068a226e
Thomas Lang <langthomas.tl@gmail.com>
2018-11-20 13:52:40 -0500

    Created new defense file

---
+++ soccer/gameplay/tactics/Adaptive_Defense_2.py
@@ -0,0 +1,13 @@
+import composite_behavior
+import behavior
+import constants
+import robocup
+import main
+import enum
+import math
+
+import tactics.positions.submissive_goalie as submissive_goalie
+import tactics.positions.submissive_defender as submissive_defender
+
+import evaluation.field
+import evaluation.linear_classification
